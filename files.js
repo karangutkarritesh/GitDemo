d9293f9a82324d83a09a84ca1341c65acb173402
ritz karan <ritz@karan.com>
2025-07-23 12:52:08 -0400

    first commit

---
+++ files.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+fs.readFile('./docs/blog1.txt', (err, data) => {
+     if(err){
+        console.log(err);
+    }
+    console.log(data.toString());
+});
+
+console.log('last line');
